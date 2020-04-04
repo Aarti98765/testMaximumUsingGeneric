d83049849184a5ff06bc3cfa8321794d0b1a8096
Aarti98765 <aartiahireengit@gmail.com>
2020-04-04 13:20:11 +0530

    ADDED: Code to given three integer find maximum

--- src/main/java/com/bridgelabz/generic/GetMaximum.java
+++ src/main/java/com/bridgelabz/generic/GetMaximum.java
@@ -1,6 +1,14 @@
 package com.bridgelabz.generic;
 
 public class GetMaximum {
+    public static  Integer findMaxInteger(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
+        Integer maximumValue = firstNumber;
+        if (secondNumber.compareTo(maximumValue) > 0)
+            maximumValue = secondNumber;
+        if (thirdNumber.compareTo(maximumValue) > 0)
+            maximumValue = thirdNumber;
+        return maximumValue;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to the program of finding maximum one among all");
     }
--- src/test/java/com/bridgelabz/generic/TestMaximum.java
+++ src/test/java/com/bridgelabz/generic/TestMaximum.java
@@ -1,5 +1,29 @@
 package com.bridgelabz.generic;
 
-public class TestMaximum {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class TestMaximum {
+    GetMaximum getMaximum;
+
+    @Test
+    public void givenIntegers_whenMaxIsFirst_returnMaximum() {
+        getMaximum = new GetMaximum();
+        int maxInteger = getMaximum.findMaxInteger(6, 4, 2);
+        Assert.assertEquals(6, maxInteger);
+    }
+
+    @Test
+    public void givenIntegers_whenMaxIsSecond_returnMaximum() {
+        getMaximum = new GetMaximum();
+        int maxInteger = getMaximum.findMaxInteger(2, 6, 4);
+        Assert.assertEquals(6, maxInteger);
+    }
+
+    @Test
+    public void givenIntegers_whenMaxIsThird_returnMaximum() {
+        getMaximum = new GetMaximum();
+        int maxInteger = getMaximum.findMaxInteger(2, 4, 6);
+        Assert.assertEquals(6, maxInteger);
+    }
 }
