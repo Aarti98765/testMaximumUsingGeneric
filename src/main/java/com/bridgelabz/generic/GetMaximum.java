50b3662d95f4828433f58b213c96da959df1b9f1
Aarti98765 <aartiahireengit@gmail.com>
2020-04-04 12:57:48 +0530

    ADDED: Welcome to the program of test maximum

---
+++ src/main/java/com/bridgelabz/generic/GetMaximum.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.generic;
+
+public class GetMaximum {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the program of finding maximum one among all");
+    }
+}
